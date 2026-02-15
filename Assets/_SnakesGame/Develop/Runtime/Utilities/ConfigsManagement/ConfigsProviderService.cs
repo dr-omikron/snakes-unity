fbe8603c90f32620d35b2202aba47261f120c527
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-02-15 15:36:00 +0400

    Created configs provider service

---
+++ Assets/_SnakesGame/Develop/Runtime/Utilities/ConfigsManagement/ConfigsProviderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+namespace _SnakesGame.Develop.Runtime.Utilities.ConfigsManagement
+{
+    public class ConfigsProviderService
+    {
+        private readonly Dictionary<Type, object> _configs = new Dictionary<Type, object>();
+        private readonly IConfigsLoader[] _configsLoaders;
+
+        public ConfigsProviderService(params IConfigsLoader[] configsLoaders)
+        {
+            _configsLoaders = configsLoaders;
+        }
+
+        public IEnumerator LoadAsync()
+        {
+            foreach (var loader in _configsLoaders)
+                yield return loader.LoadAsync(loadedConfigs => _configs.AddRange(loadedConfigs));
+        }
+
+        public T GetConfig<T>() where T : class
+        {
+            if (_configs.ContainsKey(typeof(T)) == false)
+                throw new InvalidOperationException($"Not found config by {typeof(T)}");
+
+            return (T)_configs[typeof(T)];
+        }
+    }
+}
---
+++ Assets/_SnakesGame/Develop/Runtime/Utilities/ConfigsManagement/ResourcesConfigsLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using _SnakesGame.Develop.Runtime.Utilities.AssetsManagement;
+using UnityEngine;
+
+namespace _SnakesGame.Develop.Runtime.Utilities.ConfigsManagement
+{
+    public class ResourcesConfigsLoader : IConfigsLoader
+    {
+        private readonly ResourcesAssetsLoader _resourcesAssetsLoader;
+
+        private readonly Dictionary<Type, string> _configsResourcesPaths = new Dictionary<Type, string>
+        {
+            //{}
+        };
+
+        public ResourcesConfigsLoader(ResourcesAssetsLoader resourcesAssetsLoader)
+        {
+            _resourcesAssetsLoader = resourcesAssetsLoader;
+        }
+
+        public IEnumerator LoadAsync(Action<Dictionary<Type, object>> onConfigsLoaded)
+        {
+            Dictionary<Type, object> loadedConfigs = new Dictionary<Type, object>();
+
+            foreach (KeyValuePair<Type, string> resourcesPath in _configsResourcesPaths)
+            {
+                ScriptableObject config = _resourcesAssetsLoader.Load<ScriptableObject>(resourcesPath.Value);
+                loadedConfigs.Add(resourcesPath.Key, config);
+                yield return null;
+            }
+
+            onConfigsLoaded?.Invoke(loadedConfigs);
+        }
+    }
+}
