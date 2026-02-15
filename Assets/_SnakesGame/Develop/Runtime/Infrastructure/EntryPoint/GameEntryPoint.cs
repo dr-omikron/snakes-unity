6bfdfc07391a7bdfea19d17fce8f56a48666c862
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-02-15 16:44:26 +0400

    Created game entry point

---
+++ Assets/_SnakesGame/Develop/Runtime/Infrastructure/EntryPoint/GameEntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using _SnakesGame.Develop.Runtime.Infrastructure.DI;
+using _SnakesGame.Develop.Runtime.Utilities.ConfigsManagement;
+using _SnakesGame.Develop.Runtime.Utilities.CoroutinesManagement;
+using UnityEngine;
+
+namespace _SnakesGame.Develop.Runtime.Infrastructure.EntryPoint
+{
+    public class GameEntryPoint : MonoBehaviour
+    {
+        private void Awake()
+        {
+#if PLATFORM_STANDALONE_ANDROID
+
+            SetupAndroidSetting();
+
+#elif PLATFORM_STANDALONE_WIN
+
+            SetupWindowsSetting();
+
+#endif
+
+            DIContainer container = new DIContainer();
+            ProjectContextRegistrations.Process(container);
+            container.Resolve<ICoroutinesPerformer>().StartPerform(Initialize(container));
+        }
+
+        private void SetupAndroidSetting()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+        }
+
+        private void SetupWindowsSetting()
+        {
+            //windows settings
+        }
+
+        private IEnumerator Initialize(DIContainer container)
+        {
+            yield return container.Resolve<ConfigsProviderService>().LoadAsync();
+        }
+    }
+}
---
+++ Assets/_SnakesGame/Develop/Runtime/Infrastructure/EntryPoint/ProjectContextRegistrations.cs
@@ -0,0 +1,38 @@
+﻿using _SnakesGame.Develop.Runtime.Infrastructure.DI;
+using _SnakesGame.Develop.Runtime.Utilities.AssetsManagement;
+using _SnakesGame.Develop.Runtime.Utilities.ConfigsManagement;
+using _SnakesGame.Develop.Runtime.Utilities.CoroutinesManagement;
+using UnityEngine;
+
+namespace _SnakesGame.Develop.Runtime.Infrastructure.EntryPoint
+{
+    public class ProjectContextRegistrations
+    {
+        public static void Process(DIContainer container)
+        {
+            container.RegisterAsSingle<ICoroutinesPerformer>(CreateCoroutinesPerformer);
+            container.RegisterAsSingle(CreateConfigsProviderService);
+            container.RegisterAsSingle(CreateResourcesAssetsLoader);
+        }
+
+        private static CoroutinesPerformer CreateCoroutinesPerformer(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            CoroutinesPerformer coroutinesPerformerPrefab = 
+                resourcesAssetsLoader.Load<CoroutinesPerformer>("Utilities/CoroutinesPerformer");
+
+            return Object.Instantiate(coroutinesPerformerPrefab);
+        }
+
+        private static ConfigsProviderService CreateConfigsProviderService(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+            ResourcesConfigsLoader resourcesConfigsLoader = new ResourcesConfigsLoader(resourcesAssetsLoader);
+            return new ConfigsProviderService(resourcesConfigsLoader);
+        }
+
+        private static ResourcesAssetsLoader CreateResourcesAssetsLoader(DIContainer c) => new ResourcesAssetsLoader();
+
+    }
+}
