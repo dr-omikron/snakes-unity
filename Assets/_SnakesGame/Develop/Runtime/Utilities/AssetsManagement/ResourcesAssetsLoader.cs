29f45e69b9ea9b983344c7f8b944427d1da63adf
dr-omikron <76596650+dr-omikron@users.noreply.github.com>
2026-02-15 15:16:01 +0400

    Created resources assets loader service

---
+++ Assets/_SnakesGame/Develop/Runtime/Utilities/AssetsManagement/ResourcesAssetsLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _SnakesGame.Develop.Runtime.Utilities.AssetsManagement
+{
+    public class ResourcesAssetsLoader
+    {
+        public T Load<T>(string path) where T : Object => Resources.Load<T>(path);
+    }
+}
